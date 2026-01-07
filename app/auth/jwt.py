dd6b06fc7085c3e9545e27b497a733656dbadbe4
akhgup <*Etraveli*2025@>
2026-01-07 01:19:31 +0100

    fix 1

--- app/auth/jwt.py
+++ app/auth/jwt.py
@@ -3,24 +3,23 @@ from datetime import datetime, timedelta
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
-from app.config.config import SECRET_KEY, ACCESS_TOKEN_EXPIRE_DAYS, ALGORITHM
 from jose import jwt, JWTError
 
 from app.models.user import User
 
 def create_access_token(subject: str):
-    expire = datetime.utcnow() + timedelta(days=ACCESS_TOKEN_EXPIRE_DAYS)
+    expire = datetime.utcnow() + timedelta(days=7)
     payload = {
         "sub": subject,
         "exp": expire
     }
-    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt.encode(payload, "dev-secret-key", algorithm="HS256")
 
 def get_user_from_token(token: str, db: Session):
     credentials_exception = HTTPException(status_code=401,detail="Could not validate credentials")
 
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, "dev-secret-key", algorithms=["HS256"])
         identifier: str = payload.get("sub")
         if identifier is None:
             raise credentials_exception
